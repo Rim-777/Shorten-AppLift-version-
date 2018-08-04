4c9ed7cfd06905f5953b889c6cd70b203cf28e87
Rim-777 <javatimomoss@gmail.com>
2018-08-04 16:37:39 +0300

    added test-environment and an api versioner

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  api vendor_string: 'shorten', default_version: 1 do
+    version 1 do
+      cache as: 'v1' do
+      end
+    end
+  end
 end
